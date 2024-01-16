e4e8c4b222a5a293c1385aa4b30ceaa75b1b5abb
BiaAkemi <beancaakemi@gmail.com>
2024-01-16 12:09:39 -0300

    first commit

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from 'react'
 import './Task.css'
 
 function Task() {
+  const [completado, setCompletado] = useState(false) // bolean
+  const [tarefa, setTarefa] = useState('') // string
+
+  useEffect(() => {
+    if(completado) {
+      setTarefa("Tarefa Concluída")
+    }
+
+  }, [completado])
+
+
   return (
     <div className="container">
-        <h1>Tarefa</h1>
-        <h3>Status da Tarefa</h3>
-        <button>Conclua a Tarefa</button>
+        <h1> Tarefa </h1>
+        <h3> Status da Tarefa: { tarefa } </h3>
+        <button onClick= {() => {setCompletado(true)}}> Conclua a Tarefa </button>
     </div>
   )
 }
